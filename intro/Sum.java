4cd4bcf0cd8476495ea8ca5908be116a17e280ec
deepakoffl007 <deepakelumalai.e@gmail.com>
2026-02-12 08:44:42 +0530

    java intro remember

---
+++ intro/Sum.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Sum {
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+            int a, b,c;
+            System.out.println("Enter two Numbers :");
+            a = input.nextInt();
+            b = input.nextInt();
+            c = a + b;
+            System.out.println(c);
+        }
+        
+    }
+    }
+
